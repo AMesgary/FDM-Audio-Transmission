28a2f6a7e78a7627a33406e6554eb89bf910207a
AMesgary <a.mesgary.84@gmail.com>
2026-01-31 17:28:35 +0330

    Added voice recording

---
+++ Recorder.py
@@ -0,0 +1,10 @@
+import sounddevice as sd
+from scipy.io import wavfile
+
+fs = 44100
+duration = 10
+print("Recording...")
+audio = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+sd.wait()
+wavfile.write("audio_1.wav", fs, audio)
+print("Finished!")
